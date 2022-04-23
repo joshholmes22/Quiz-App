89e0bb97eb5eed7231d126debcb144266957b6b3
Josh Holmes <josh@glowingmanagement.com>
2022-04-23 09:34:21 +0100

    load question screen on button press

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,17 @@
 const startButton = document.getElementById("start-quiz");
 
 const startQuiz = () => {
-  console.log("Quiz starts");
+  // get title screen
+  const titleScreen = document.getElementById("title-screen");
+  titleScreen.classList.add("hide");
+  // get question screen
+  const questionScreen = document.getElementById("question-screen");
+  questionScreen.classList.remove("hide");
+
+  // display question
 };
+
+const getQuestion = () => {};
 
 // add click event listener
 startButton.addEventListener("click", startQuiz);
