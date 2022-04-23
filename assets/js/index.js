dbb0b4b18ca60ee07efa079af104ccb7c0a784e9
Josh Holmes <josh@glowingmanagement.com>
2022-04-23 09:40:43 +0100

    change question text in javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,9 +10,52 @@ const startQuiz = () => {
   questionScreen.classList.remove("hide");
 
   // display question
+  getQuestion();
 };
 
-const getQuestion = () => {};
+const getQuestion = () => {
+  const questionText = document.getElementById("question-text");
+  questionText.textContent = "Javascript text";
+};
+
+const questions = [
+  {
+    question: "Question 1 goes here",
+    answers: [
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false },
+      { text: "right", correct: true },
+      { text: "wrong", correct: false },
+    ],
+  },
+  {
+    question: "Question 2 goes here",
+    answers: [
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false },
+      { text: "right", correct: true },
+      { text: "wrong", correct: false },
+    ],
+  },
+  {
+    question: "Question 3 goes here",
+    answers: [
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false },
+      { text: "right", correct: true },
+      { text: "wrong", correct: false },
+    ],
+  },
+  {
+    question: "Question 4 goes here",
+    answers: [
+      { text: "wrong", correct: false },
+      { text: "wrong", correct: false },
+      { text: "right", correct: true },
+      { text: "wrong", correct: false },
+    ],
+  },
+];
 
 // add click event listener
 startButton.addEventListener("click", startQuiz);
